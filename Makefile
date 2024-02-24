f033b628084152785ca04e4d28d1ea81415b6f2d
pedperei <pedrob1095@hotmail.com>
2024-02-24 16:44:55 +0000

    wordpress test

---
+++ Makefile
@@ -0,0 +1,57 @@
+# Name of the project
+NAME = inception
+
+# Build the Docker containers defined in the docker-compose.yml file
+build:
+# -f: Specify an alternate compose file
+# --build: Build images before starting containers
+# -d: Detached mode: Run containers in the background
+	docker-compose -f srcs/docker-compose.yml up -d --build
+
+# Stop and remove the Docker containers defined in the docker-compose.yml file
+down:
+	docker-compose -f srcs/docker-compose.yml down
+
+start:
+	docker-compose -f srcs/docker-compose.yml start
+
+stop:
+	docker-compose -f srcs/docker-compose.yml stop
+
+clean:
+	docker-compose -f srcs/docker-compose.yml down --volumes --rmi all
+
+fclean: clean
+# prune: Remove unused data
+# -a: Remove all unused images not just dangling ones
+# --volumes: Prune volumes
+# --force: Do not prompt for confirmation
+	docker system prune -a --volumes --force
+	docker network ls -q -f "driver=custom" | xargs -r docker network rm 2>/dev/null
+	sudo rm -rf /home/pedperei/data/mysql/*
+	sudo rm -rf /home/pedperei/data/wordpress/*
+
+re: fclean build
+
+info:
+	@echo "Containers:"
+	@docker ps -a
+	@echo "------------------------------------------------------------"
+	@echo "Images:"
+	@docker images -a
+	@echo "------------------------------------------------------------"
+	@echo "Volumes:"
+	@docker volume ls
+	@echo "------------------------------------------------------------"
+	@echo "Networks:"
+	@docker network ls
+	@echo "------------------------------------------------------------"
+
+connect-mariadb:
+	docker exec -it mariadb mysql -u root -p
+
+connect-wordpress:
+	docker exec -it wordpress /bin/bash
+
+# Declare the targets as phony to avoid conflicts with file names
+.PHONY: build up down clean fclean re info connect
---
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+sleep 3
+
+
+# Define the path to the WordPress configuration file
+conf_file="/var/www/html/wp-config.php"
+
+# Function to download WordPress core files
+wp_download()
+{
+    if wp core download --allow-root; 
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
+# Function to create the wordpress configuration (wp-config.php)
+wp_config() 
+{
+    if wp config create \
+        --allow-root \
+        --path=/var/www/html/ \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASS \
+        --dbhost=$MYSQL_HOST
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
+# Function to install WordPress
+wp_install()
+{
+    if wp core install    \
+        --allow-root \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_USER_PASSWORD \
+        --admin_email=$WP_ADMIN_MAIL \
+        --url=$WP_URL 
+    then
+        wp user create \
+        --allow-root \
+        $WP_USER $WP_USER_EMAIL \
+        --user_pass=$WP_ADMIN_PASSWORD
+        return 1
+    else
+        return 0
+    fi
+}
+
+# This section of the shell script checks if the file specified by the variable `` exists.
+    # Initialization
+    command=0
+    attempts=0
+    max_attempts=5
+    
+    # change directory to WordPress installation path
+    cd /var/www/html/
+
+    # Loop until the file is downloaded or the number of attempts 
+    # has exceeded the maximum 
+    while [ $attempts -le $max_attempts ]; do
+
+        if [ $command -eq 0 ]; then
+            echo "Downloading Wordpress..."
+            wp_download
+            ((command+=1))
+        fi
+        if [ $command -eq 1 ]; then
+            echo "Configuring Wordpress..."
+            wp_config
+            ((command+=1))
+        fi
+        if [ $command -eq 2 ]; then
+            echo "Installing Wordpress..."
+            wp_install
+            ((command+=1))
+        fi
+        if [ $command -ge 3 ]; then
+            break
+        fi
+        ((attempts+=1))
+
+        sleep 1
+
+    done
+    
+    if [ $attempts -ge $max_attempts ]; then
+        echo "Failed to install Wordpress"
+    else
+        echo "Wordpress installed successfully"
+    fi
+  
+
+exec "$@"
